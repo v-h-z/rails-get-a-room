5bd94d9382d73e851cd389f2a6e4fe63b24bcd14
victor holl <v_h@me.com>
2020-05-22 15:47:07 +0200

    add name to Room

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,8 +1,10 @@
 class RoomsController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
   def index
+    @rooms = Room.all
   end
 
   def show
+    @room = Room.find(params[:id])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_22_132959) do
+ActiveRecord::Schema.define(version: 2020_05_22_134536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2020_05_22_132959) do
     t.boolean "independant"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
   end
 
   create_table "types", force: :cascade do |t|
