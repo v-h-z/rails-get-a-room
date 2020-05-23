1fbd4a52e08a572375fe6ee973d550e8de2f6acb
victor holl <v_h@me.com>
2020-05-23 16:13:08 +0200

    add room new

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -7,4 +7,25 @@ class RoomsController < ApplicationController
   def show
     @room = Room.find(params[:id])
   end
+
+  def new
+    @room = Room.new
+  end
+
+  def create
+    # @room = current_user.rooms.build(room_params)
+    @room = Room.new(room_params)
+    @room.user = current_user
+    if @room.save
+      redirect_to room_path(@room)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name, :adress, :superficy, :independant)
+  end
 end
