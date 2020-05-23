354480777ed16549eca20c6a11b768ed636c28b7
victor holl <v_h@me.com>
2020-05-23 17:36:03 +0200

    refacto set_room

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,21 +1,23 @@
 class RoomsController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
+  before_action :set_room, only: [:show, :edit, :update, :destroy]
   def index
     @rooms = Room.all
   end
 
   def show
-    @room = Room.find(params[:id])
   end
 
   def new
     @room = Room.new
+    authorize @room
   end
 
   def create
     # @room = current_user.rooms.build(room_params)
     @room = Room.new(room_params)
     @room.user = current_user
+    authorize @room
     if @room.save
       redirect_to room_path(@room)
     else
@@ -23,7 +25,24 @@ class RoomsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    @room.update(room_params)
+    if @room.save
+      redirect_to room_path(@room)
+    else
+      render :edit
+    end
+  end
+
   private
+
+  def set_room
+    @room = Room.find(params[:id])
+    authorize @room
+  end
 
   def room_params
     params.require(:room).permit(:name, :adress, :superficy, :independant)
---
+++ app/policies/room_policy.rb
@@ -0,0 +1,19 @@
+class RoomPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    current_user == user
+  end
+end
