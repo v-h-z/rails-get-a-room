b228e05a813d675cfd29b9056239af0d8cd8ce65
victor holl <v_h@me.com>
2020-05-24 17:27:14 +0200

    still trying to fix map touch controls

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -30,9 +30,10 @@ const initMapbox = () => {
       style: 'mapbox://styles/mapbox/streets-v10'
     });
     map.scrollZoom.disable();
+    map.touchZoomRotate.enable({ around: 'center' });
     map.addControl(new mapboxgl.NavigationControl());
-    if (map.tap) map.tap.disable();
-    if (map.tap) map.dragging.disable();
+    // if (map.tap) map.tap.disable();
+    // if (map.tap) map.dragging.disable();
 
     const markers = JSON.parse(mapElement.dataset.markers);
 
